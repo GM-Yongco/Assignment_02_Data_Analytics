a56c5a0526649271fa3dcdb85cbab906e49d5777
Giulio Yongco <yongcogiulio125@gmail.com>
2025-02-09 03:31:33 +0800

    graph and color done

--- network_graph.py
+++ network_graph.py
@@ -4,6 +4,13 @@
 # Actual Description	: Code that will impress u ;)
 # HEADERS ================================================================
 
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from typing import List
+
 # ========================================================================
 # FUNCTIONS MISC
 # ========================================================================
@@ -24,4 +31,34 @@ def section(section_name:str = "SECTION") -> None:
 
 if __name__ == '__main__':
 	section("START")
+
+	nodes_blue:List[str] = ['D','F','I','N','S']
+	nodes_green:List[str] = ['BIH', 'GEO', 'ISR', 'MNE', 'SRB', 'CHE', 'TUR', 'UKR', 'GBR', 'AUS', 'HKG', 'USA']
+	nodes_yellow:List[str] = ['AUT', 'BEL', 'BGR', 'HRV', 'CZE', 'EST', 'FRA', 'DEU', 'GRC', 'HUN', 'IRL', 'ITA', 'LVA', 'LUX', 'NLD', 'PRT', 'ROU', 'SVK', 'SVN', 'ESP']
+
+	file_name:str = r"references/networks_assignment.csv"
+	df:pd.DataFrame = pd.read_csv(file_name, index_col=0)
+
+	G:nx.DiGraph = nx.DiGraph()
+
+	for col in df.columns:
+		col:str = str(col)
+		color_str:str = "yellow"
+		if col in nodes_blue:
+			color_str = "blue"
+		elif col in nodes_green:
+			color_str = "green"
+		G.add_node(col, color=color_str)
+
+	for col in df.columns:
+		for row in df[col].index:
+			edge_weight:int = df.at[row, col]
+			if edge_weight > 0:
+				# print(f"{col}, {row}, {df.at[row, col]}")
+				G.add_edge(col, row, weight = edge_weight)
+
+	node_colors = [G.nodes[n]['color'] for n in G.nodes]
+	nx.draw(G, with_labels=True, node_color=node_colors)
+	plt.show()
+
 	section("END")
