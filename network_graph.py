8f46e83a505cace6ea5ff71b3e130b3ef37148a0
Giulio Yongco <yongcogiulio125@gmail.com>
2025-02-09 00:27:49 +0800

    initial commit

---
+++ network_graph.py
@@ -0,0 +1,27 @@
+# Author				: G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
+# Date					: ur my date uwu
+# Description			: Code that will impress u ;)
+# Actual Description	: Code that will impress u ;)
+# HEADERS ================================================================
+
+# ========================================================================
+# FUNCTIONS MISC
+# ========================================================================
+
+def section(section_name:str = "SECTION") -> None:
+	section_name = f"\n {section_name} {'-' * (40 - len(section_name))}\n"
+	print(section_name)
+
+# ========================================================================
+# FUNCTIONS 
+# ========================================================================
+
+
+
+# ========================================================================
+# MAIN 
+# ========================================================================
+
+if __name__ == '__main__':
+	section("START")
+	section("END")
